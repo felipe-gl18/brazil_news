2ae1c9f0d0285e056a22a54550ae2e7db1b40607
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-06 11:08:23 -0300

    feat: add CreateUser use case
    Added CreateUserDTO for input typing, Transform string email/whatsapp into Values Objects, Implemented repository dependency inversion, Added a complete unit test for happy path and validation erros

--- src/application/useCases/CreateUser.ts
+++ src/application/useCases/CreateUser.ts
@@ -1,8 +1,27 @@
-interface Request {
-  name: string;
-  email: string;
-  whatsapp?: string;
-  topics: string[];
-}
+import { User } from "../../domain/entities/User";
+import { IUserRepository } from "../../domain/repositories/IUserRepository";
+import { Email } from "../../domain/valueObjects/Email";
+import { WhatsApp } from "../../domain/valueObjects/WhatsApp";
+import { CreateUserDTO } from "./CreateUserDTO";
 
-export class CreateUser {}
+export class CreateUser {
+  constructor(private readonly userRepository: IUserRepository) {}
+
+  async execute(data: CreateUserDTO) {
+    // transforming string in Value Objects
+    const email = new Email(data.email);
+    const whatsapp = data.whatsapp ? new WhatsApp(data.whatsapp) : undefined;
+
+    // creating user entity
+    // id is generated automatically by the database
+    const user = new User({
+      name: data.name,
+      email,
+      whatsapp,
+      topics: data.topics,
+    });
+
+    // saving user in the repository
+    await this.userRepository.create(user);
+  }
+}
