a0f1499ccaa72de208210eb460df6a7fbf509af1
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-08 10:35:28 -0300

    chore: add more required functions, change props
    new functions such as findById, and props at updateUserTopics went from email to id

--- src/domain/repositories/IUserRepository.ts
+++ src/domain/repositories/IUserRepository.ts
@@ -3,6 +3,7 @@ import { User } from "../entities/User";
 export interface IUserRepository {
   create(user: User): Promise<void>;
   findByEmail(email: string): Promise<User | null>;
+  findById(id: string): Promise<User | null>;
   updateUserTopics(id: string, topics: string[]): Promise<void>;
   deleteById(id: string): Promise<void>;
 }
