63a74b7c13b68b40c6f8f956d61bf6dd013b21b6
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-09 19:20:56 -0300

    chore: change userRespository to userRepository

--- src/application/useCases/DeleteUser.ts
+++ src/application/useCases/DeleteUser.ts
@@ -1,17 +1,17 @@
 import { IUserRepository } from "../../domain/repositories/IUserRepository";
 
 export class DeleteUser {
-  constructor(private readonly userRespository: IUserRepository) {}
+  constructor(private readonly userRepository: IUserRepository) {}
 
   async execute(id: string) {
     // validating if the user at least exist before deleting
-    const foundUser = await this.userRespository.findById(id);
+    const foundUser = await this.userRepository.findById(id);
     if (!foundUser) throw new Error("User does not exist!");
 
     // error handling
     try {
       // deleting user in the repository
-      await this.userRespository.deleteById(id);
+      await this.userRepository.deleteById(id);
     } catch (error) {
       if (error instanceof Error)
         throw new Error(`Something went wrong: ${error.message}`);
