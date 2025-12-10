a30571ff256e73ccec76a0431217508f2a87234f
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-10 19:04:47 -0300

    chore: add DeliveredNews Entity
    DeliveredNews will be news that will be saved on the database

---
+++ src/domain/entities/DeliveredNews.ts
@@ -0,0 +1,30 @@
+interface DeliveredNewsProps {
+  link: string;
+  topic: string;
+  userId: string;
+  sentAt: Date;
+}
+export class DeliveredNews {
+  private props: DeliveredNewsProps;
+  private _id?: string;
+
+  constructor(props: DeliveredNewsProps) {
+    if (!props.link?.trim()) throw new Error("Link cannot be empty");
+    if (!props.topic?.trim()) throw new Error("Topic cannot be empty");
+    if (!props.userId?.trim()) throw new Error("UserId cannot be empty");
+    this.props = props;
+  }
+
+  get link() {
+    return this.props.link;
+  }
+  get topic() {
+    return this.props.topic;
+  }
+  get userId() {
+    return this.props.userId;
+  }
+  get sentAt() {
+    return this.props.sentAt;
+  }
+}
