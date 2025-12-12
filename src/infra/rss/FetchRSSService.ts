a8548739f613dc0e70282de1e4fa7cb28ef90670
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-12 18:09:45 -0300

    feat: FetchRSSService wont be used now

--- src/infra/rss/FetchRSSService.ts
+++
@@ -1,6 +0,0 @@
-import { IFetchRSSService } from "../../application/services/IFetchRSSService";
-import { News } from "../../domain/entities/News";
-
-export class FetchRSSService implements IFetchRSSService {
-  async fetchLatestNews(topics: string[]): Promise<News[]> {}
-}
