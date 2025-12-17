517b62a72b0926b68ba6120e6d7df076a4356d02
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-17 19:58:02 -0300

    chore: add proper error handling
    capturing prisma erros, and adding custom erros such as UserNotFoundError

--- src/infra/prisma/DeliveredNewsRepositoryPrisma.ts
+++ src/infra/prisma/DeliveredNewsRepositoryPrisma.ts
@@ -1,50 +1,14 @@
 import { PrismaClient, Prisma } from "../../../generated/prisma/client";
 import { DeliveredNews } from "../../domain/entities/DeliveredNews";
+import { LinkAlreadyInUseError } from "../../domain/erros/LinkAlreadyInUse";
 import { UserNotFoundError } from "../../domain/erros/UserNotFoundError";
 import { IDeliveredNewsRepository } from "../../domain/repositories/IDeliveredNewsRepository";
+import { DatabaseError } from "../errors/DatabaseError";
 import { RepositoryError } from "../errors/RepositoryError";
 import { DeliveredNewsMapper } from "../mappers/DeliveredNewsMapper";
 
 export class DeliveredNewsRepositoryPrisma implements IDeliveredNewsRepository {
   constructor(private readonly prismaClient: PrismaClient) {}
-  async create(deliveredNews: DeliveredNews): Promise<void> {
-    try {
-      await this.prismaClient.deliveredNews.create({
-        data: deliveredNews,
-      });
-    } catch (error: any) {
-      if (
-        error instanceof Prisma.PrismaClientKnownRequestError &&
-        error.code === "P2002"
-      ) {
-        throw new RepositoryError(
-          "Delivered news violates a unique constraint",
-          error
-        );
-      }
-      throw new RepositoryError("Failed to create delivered news", error);
-    }
-  }
-  async deleteByUser(userId: string): Promise<void> {
-    try {
-      await this.prismaClient.deliveredNews.deleteMany({
-        where: {
-          userId,
-        },
-      });
-    } catch (error: any) {
-      if (
-        error instanceof Prisma.PrismaClientKnownRequestError &&
-        error.code === "P2025"
-      ) {
-        throw new UserNotFoundError();
-      }
-      throw new RepositoryError(
-        "Failed to delete delivered news from user",
-        error
-      );
-    }
-  }
   async findAll(): Promise<DeliveredNews[] | null> {
     try {
       const deliveredNews = await this.prismaClient.deliveredNews.findMany();
@@ -53,7 +17,9 @@ export class DeliveredNewsRepositoryPrisma implements IDeliveredNewsRepository {
         DeliveredNewsMapper.toDomain(deliveredNew)
       );
     } catch (error: any) {
-      throw new RepositoryError("Failed to find all delivered news", error);
+      if (error instanceof Prisma.PrismaClientInitializationError)
+        throw new DatabaseError("Failed to find delivered news", error);
+      throw new RepositoryError("Failed to find delivered news", error);
     }
   }
   async findByUser(userId: string): Promise<DeliveredNews[] | null> {
@@ -69,7 +35,36 @@ export class DeliveredNewsRepositoryPrisma implements IDeliveredNewsRepository {
         DeliveredNewsMapper.toDomain(deliveredNew)
       );
     } catch (error: any) {
-      throw new RepositoryError("Failed to find all delivered news", error);
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === "P2025"
+      )
+        throw new UserNotFoundError();
+      if (error instanceof Prisma.PrismaClientInitializationError)
+        throw new DatabaseError(
+          "Failed to find delivered news from user",
+          error
+        );
+      throw new RepositoryError(
+        "Failed to find delivered news from user",
+        error
+      );
+    }
+  }
+  async save(deliveredNews: DeliveredNews): Promise<void> {
+    try {
+      await this.prismaClient.deliveredNews.create({
+        data: deliveredNews,
+      });
+    } catch (error: any) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === "P2002"
+      )
+        throw new LinkAlreadyInUseError(deliveredNews.link);
+      if (error instanceof Prisma.PrismaClientInitializationError)
+        throw new DatabaseError("Failed to save delivered news", error);
+      throw new RepositoryError("Failed to save delivered news", error);
     }
   }
   async saveMany(deliveredNews: DeliveredNews[]): Promise<void> {
@@ -81,13 +76,35 @@ export class DeliveredNewsRepositoryPrisma implements IDeliveredNewsRepository {
       if (
         error instanceof Prisma.PrismaClientKnownRequestError &&
         error.code === "P2002"
-      ) {
-        throw new RepositoryError(
-          "One or more delivered news entries violates a unique constraint",
+      )
+        throw new LinkAlreadyInUseError("");
+      if (error instanceof Prisma.PrismaClientInitializationError)
+        throw new DatabaseError("Failed to save many delivered news", error);
+      throw new RepositoryError("Failed to save many delivered news", error);
+    }
+  }
+  async deleteByUser(userId: string): Promise<void> {
+    try {
+      await this.prismaClient.deliveredNews.deleteMany({
+        where: {
+          userId,
+        },
+      });
+    } catch (error: any) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === "P2025"
+      )
+        throw new UserNotFoundError();
+      if (error instanceof Prisma.PrismaClientInitializationError)
+        throw new DatabaseError(
+          "Failed to delete delivered news from user",
           error
         );
-      }
-      throw new RepositoryError("Failed to save many delivered news", error);
+      throw new RepositoryError(
+        "Failed to delete delivered news from user",
+        error
+      );
     }
   }
 }
