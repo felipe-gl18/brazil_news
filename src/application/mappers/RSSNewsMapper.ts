b7255df7375ebdf7fe69083c5589f2ea62635cc7
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-17 19:51:24 -0300

    chore: add RSSNewsMapper

--- src/application/mappers/RSSNewsMapper.ts
+++ src/application/mappers/RSSNewsMapper.ts
@@ -1,5 +1,5 @@
-import { DeliveredNews } from "../../domain/entities/DeliveredNews";
-import { News } from "../../domain/entities/News";
+import { DeliveredNews } from "../../domain/entities/DeliveredNews.js";
+import { News } from "../../domain/entities/News.js";
 
 export class RSSNewsMapper {
   static toDeliveredNews(rssNews: News, userId: string): DeliveredNews {
