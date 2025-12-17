95f4a9a698c6747be31e792c926e39700d5f6cd3
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-17 19:54:54 -0300

    chore: remove unnecessary error handling and empty lines

--- src/application/useCases/DeleteUser.ts
+++ src/application/useCases/DeleteUser.ts
@@ -1,21 +1,7 @@
-import { IUserRepository } from "../../domain/repositories/IUserRepository";
-
+import { IUserRepository } from "../../domain/repositories/IUserRepository.js";
 export class DeleteUser {
   constructor(private readonly userRepository: IUserRepository) {}
-
   async execute(id: string) {
-    // validating if the user at least exist before deleting
-    const foundUser = await this.userRepository.findById(id);
-    if (!foundUser) throw new Error("User does not exist!");
-
-    // error handling
-    try {
-      // deleting user in the repository
-      await this.userRepository.deleteById(id);
-    } catch (error) {
-      if (error instanceof Error)
-        throw new Error(`Something went wrong: ${error.message}`);
-      throw new Error(`Something went wrong: ${error}`);
-    }
+    await this.userRepository.deleteById(id);
   }
 }
