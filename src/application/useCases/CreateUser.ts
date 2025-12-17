f561f056ec1f9346940543c5eb4fed60468d7746
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-17 19:53:27 -0300

    chore: remove unnecessary error handling and empty lines

--- src/application/useCases/CreateUser.ts
+++ src/application/useCases/CreateUser.ts
@@ -3,36 +3,17 @@ import { IUserRepository } from "../../domain/repositories/IUserRepository";
 import { Email } from "../../domain/valueObjects/Email.js";
 import { WhatsApp } from "../../domain/valueObjects/WhatsApp.js";
 import { CreateUserDTO } from "./CreateUserDTO";
-
 export class CreateUser {
   constructor(private readonly userRepository: IUserRepository) {}
-
   async execute(data: CreateUserDTO) {
-    // transforming string in Value Objects
     const email = new Email(data.email);
     const whatsapp = data.whatsapp ? new WhatsApp(data.whatsapp) : undefined;
-
-    // validating if an user is not using the same email
-    const foundUser = await this.userRepository.findByEmail(email.valueOf);
-    if (foundUser) throw new Error("Email already beign used");
-
-    // creating user entity
-    // id is generated automatically by the database
     const user = new User({
       name: data.name,
       email,
       whatsapp,
       topics: data.topics,
     });
-
-    // error handling
-    try {
-      // saving user in the repository
-      await this.userRepository.create(user);
-    } catch (error) {
-      if (error instanceof Error)
-        throw new Error(`Something went wrong: ${error.message}`);
-      throw new Error(`Something went wrong ${error}`);
-    }
+    await this.userRepository.create(user);
   }
 }
