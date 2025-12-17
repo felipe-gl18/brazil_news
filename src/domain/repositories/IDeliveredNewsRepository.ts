e3642d9f4e8d4fd1ffc494ea6b462dedda629486
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-17 19:56:20 -0300

    chore: rename create method to save method

--- src/domain/repositories/IDeliveredNewsRepository.ts
+++ src/domain/repositories/IDeliveredNewsRepository.ts
@@ -2,7 +2,7 @@ import { DeliveredNews } from "../entities/DeliveredNews.js";
 export interface IDeliveredNewsRepository {
   findAll(): Promise<DeliveredNews[] | null>;
   findByUser(userId: string): Promise<DeliveredNews[] | null>;
-  create(deliveredNews: DeliveredNews): Promise<void>;
+  save(deliveredNews: DeliveredNews, userId: string): Promise<void>;
+  saveMany(deliveredNews: DeliveredNews[], userId: string): Promise<void>;
   deleteByUser(userId: string): Promise<void>;
-  saveMany(deliveredNews: DeliveredNews[]): Promise<void>;
 }
