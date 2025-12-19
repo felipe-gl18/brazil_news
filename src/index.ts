64e407881bc80937b17f6f642a10ef57d534785c
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-19 11:40:13 -0300

    feat: testing all use cases scenarios

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,44 @@
-import { EmailNotificationService } from "./infra/services/EmailNotificationService.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client.js";
+import { CreateUser } from "./application/useCases/CreateUser.js";
+import { UserRepositoryPrisma } from "./infra/prisma/UserRepositoryPrisma.js";
+import "dotenv/config.js";
+import { FindUser } from "./application/useCases/FindUser.js";
+import { UpdateUserTopcis } from "./application/useCases/UpdateUserTopics.js";
+import { SendRSSNewsToUser } from "./application/useCases/SendRSSNewsToUser.js";
+import { DeliveredNewsRepositoryPrisma } from "./infra/prisma/DeliveredNewsRepositoryPrisma.js";
 import { RSSFetchNewsService } from "./infra/services/RSSFetchNewsService.js";
-import { TelegramNotificationService } from "./infra/services/TelegramNotificationService.js";
-
-const rssFetchNewsService = new RSSFetchNewsService();
+import { EmailNotificationService } from "./infra/services/EmailNotificationService.js";
+import { FindUserDeliveredNews } from "./application/useCases/FindUserDeliveredNews.js";
+import { FindDeliveredNews } from "./application/useCases/FindDeliveredNews.js";
+import { DeleteUser } from "./application/useCases/DeleteUser.js";
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+const prismaClient = new PrismaClient({ adapter });
+const userRepository = new UserRepositoryPrisma(prismaClient);
+const deliveredNewsRepository = new DeliveredNewsRepositoryPrisma(prismaClient);
+const rSSFetchNewsService = new RSSFetchNewsService();
 const emailNotificationService = new EmailNotificationService();
-const telegramNotificationService = new TelegramNotificationService();
-(async () => {
-  const news = await rssFetchNewsService.fetchLatestNews(["technology"]);
-  await emailNotificationService.notify({
-    news,
-    recipient: { email: "felipegadelha2004@gmail.com" },
-  });
-  await telegramNotificationService.notify({
-    news,
-    recipient: { telegramChatId: "5666871731" },
-  });
-})();
+const createUser = new CreateUser(userRepository);
+const findUser = new FindUser(userRepository);
+const updateUserTopics = new UpdateUserTopcis(userRepository);
+const sendRSSNewsToUser = new SendRSSNewsToUser(
+  userRepository,
+  deliveredNewsRepository,
+  rSSFetchNewsService,
+  emailNotificationService
+);
+const findUserDeliveredNews = new FindUserDeliveredNews(
+  deliveredNewsRepository
+);
+const findDeliveredNews = new FindDeliveredNews(deliveredNewsRepository);
+const deleteUser = new DeleteUser(userRepository);
+// create user
+// find user
+// update user topics
+// sendRSSNews
+// find user delivered news
+// find all delivered news
+// delete user
+(async () => {})();
