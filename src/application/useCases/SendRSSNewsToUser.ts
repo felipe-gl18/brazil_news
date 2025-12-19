4c4d31d91628229cf5df09a349f877b6266ad6b7
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-18 22:04:36 -0300

    chore: fix importing and sintax erros

--- src/application/useCases/SendRSSNewsToUser.ts
+++ src/application/useCases/SendRSSNewsToUser.ts
@@ -1,49 +1,37 @@
-import { News } from "../../domain/entities/News";
+import { News } from "../../domain/entities/News.js";
 import { IDeliveredNewsRepository } from "../../domain/repositories/IDeliveredNewsRepository";
 import { IUserRepository } from "../../domain/repositories/IUserRepository";
 import { NewsFilterService } from "../../domain/services/NewsFilterService.js";
 import { RSSNewsMapper } from "../mappers/RSSNewsMapper.js";
-import { IFetchRSSService } from "../services/IFetchRSSService";
+import { IFetchNewsService } from "../services/IFetchNewsService";
 import { INotificationService } from "../services/INotificationService";
 export class SendRSSNewsToUser {
   constructor(
     private readonly userRepository: IUserRepository,
     private readonly deliveredNewsRepository: IDeliveredNewsRepository,
-    private readonly fetchRSSService: IFetchRSSService,
+    private readonly fetchNewsService: IFetchNewsService,
     private readonly notificationService: INotificationService
   ) {}
   async execute(userId: string) {
     const foundUser = await this.userRepository.findById(userId);
-    const fetchedNews = await this.fetchRSSService.fetchLatestNews(
+    const fetchedNewsDTOs = await this.fetchNewsService.fetchLatestNews(
       foundUser!.topics
     );
-    const releventNews = fetchedNews.filter((news) =>
+    const newsEntities = fetchedNewsDTOs.map((item) => new News(item));
+    const releventNews = newsEntities.filter((news) =>
       NewsFilterService.matchUserInterests(foundUser!, news)
     );
     const deliveredNews = releventNews.map((news) =>
       RSSNewsMapper.toDeliveredNews(news, userId)
     );
     await this.deliveredNewsRepository.saveMany(deliveredNews, userId);
-    const message = this.formatNewsMessage(releventNews);
-    await this.notificationService.sendNewsEmail(
-      foundUser!.email.valueOf,
-      message
-    );
-    if (foundUser!.whatsapp) {
-      await this.notificationService.sendNewsWhatsApp(
-        foundUser!.whatsapp.valueOf,
-        message
-      );
-    }
-  }
-  private formatNewsMessage(news: News[]): string {
-    return news
-      .map(
-        (n) =>
-          `📰 ${n.title}\n${
-            n.content
-          }\nPublicado: ${n.publishedAt.toISOString()}\n`
-      )
-      .join("\n");
+    const recipient = {
+      email: foundUser!.email.valueOf,
+      telegramChatId: foundUser!.whatsapp?.valueOf,
+    };
+    await this.notificationService.notify({
+      news: releventNews,
+      recipient,
+    });
   }
 }
