fda3612b51fb5a54116bc77316fad8e03628fc78
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-19 11:39:29 -0300

    chore: remove whatsapp related code and adding telegramChatId

--- src/application/useCases/CreateUser.ts
+++ src/application/useCases/CreateUser.ts
@@ -1,17 +1,19 @@
 import { User } from "../../domain/entities/User.js";
 import { IUserRepository } from "../../domain/repositories/IUserRepository";
 import { Email } from "../../domain/valueObjects/Email.js";
-import { WhatsApp } from "../../domain/valueObjects/WhatsApp.js";
+import { TelegramChatId } from "../../domain/valueObjects/TelegramChatId.js";
 import { CreateUserDTO } from "./CreateUserDTO";
 export class CreateUser {
   constructor(private readonly userRepository: IUserRepository) {}
   async execute(data: CreateUserDTO) {
     const email = new Email(data.email);
-    const whatsapp = data.whatsapp ? new WhatsApp(data.whatsapp) : undefined;
+    const telegramChatId = data.telegramChatId
+      ? new TelegramChatId(data.telegramChatId)
+      : undefined;
     const user = new User({
       name: data.name,
       email,
-      whatsapp,
+      telegramChatId,
       topics: data.topics,
     });
     await this.userRepository.create(user);
