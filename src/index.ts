8b6219cb7bad7bce97b0c7de60046c9324d1bee5
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-18 22:05:30 -0300

    feat: testing codes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, Brazil News!");
+import { EmailNotificationService } from "./infra/services/EmailNotificationService.js";
+import { RSSFetchNewsService } from "./infra/services/RSSFetchNewsService.js";
+import { TelegramNotificationService } from "./infra/services/TelegramNotificationService.js";
+
+const rssFetchNewsService = new RSSFetchNewsService();
+const emailNotificationService = new EmailNotificationService();
+const telegramNotificationService = new TelegramNotificationService();
+(async () => {
+  const news = await rssFetchNewsService.fetchLatestNews(["technology"]);
+  await emailNotificationService.notify({
+    news,
+    recipient: { email: "felipegadelha2004@gmail.com" },
+  });
+  await telegramNotificationService.notify({
+    news,
+    recipient: { telegramChatId: "5666871731" },
+  });
+})();
