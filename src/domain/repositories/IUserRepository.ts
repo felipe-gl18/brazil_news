ed7041b510cc831b15da2e7b0cb5580eb094eacf
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-21 23:23:29 -0300

    chore: change User | null return to User only
    we're using findUniqueOrThrow, so it'll always return a user

--- src/domain/repositories/IUserRepository.ts
+++ src/domain/repositories/IUserRepository.ts
@@ -3,8 +3,8 @@ import { User } from "../entities/User.js";
 
 export interface IUserRepository {
   create(user: User, encrypted?: EncrytedPayload): Promise<void>;
-  findByEmail(email: string): Promise<User | null>;
-  findById(id: string): Promise<User | null>;
-  updateUserTopics(id: string, topics: string[]): Promise<void>;
+  findByEmail(email: string): Promise<User>;
+  findById(id: string): Promise<User>;
+  save(user: User): Promise<void>;
   deleteById(id: string): Promise<void>;
 }
