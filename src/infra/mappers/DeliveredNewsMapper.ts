5d0ab568c6b2205861d59b3fb63bd923d25ac01c
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-21 23:24:27 -0300

    fix: add raw.id as second parameter so _id has value

--- src/infra/mappers/DeliveredNewsMapper.ts
+++ src/infra/mappers/DeliveredNewsMapper.ts
@@ -2,12 +2,15 @@ import { DeliveredNews } from "../../domain/entities/DeliveredNews.js";
 
 export class DeliveredNewsMapper {
   static toDomain(raw: any) {
-    return new DeliveredNews({
-      link: raw.link,
-      topic: raw.topic,
-      userId: raw.userId,
-      sentAt: raw.sentAt,
-    });
+    return new DeliveredNews(
+      {
+        link: raw.link,
+        topic: raw.topic,
+        userId: raw.userId,
+        sentAt: raw.sentAt,
+      },
+      raw.id
+    );
   }
   static toPersistence(deliveredNews: DeliveredNews) {
     return {
