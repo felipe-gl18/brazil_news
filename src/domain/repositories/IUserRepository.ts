de18021834814f1b189be3ba7b8cd65a599b7f54
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-22 20:14:06 -0300

    chore: add findAll and findUserToNotify methods

--- src/domain/repositories/IUserRepository.ts
+++ src/domain/repositories/IUserRepository.ts
@@ -3,8 +3,10 @@ import { User } from "../entities/User.js";
 
 export interface IUserRepository {
   create(user: User, encrypted?: EncrytedPayload): Promise<void>;
+  findAll(): Promise<User[] | null>;
   findByEmail(email: string): Promise<User>;
   findById(id: string): Promise<User>;
+  findUsersToNotify(now: Date): Promise<User[] | null>;
   save(user: User): Promise<void>;
   deleteById(id: string): Promise<void>;
 }
