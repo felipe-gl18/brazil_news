84bb081ea1e694f8b58e3823e93763d4266f4819
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-21 23:18:48 -0300

    chore: remove UpdateUserTopics
    added UpdateUser

--- src/application/useCases/UpdateUserTopics.ts
+++
@@ -1,7 +0,0 @@
-import { IUserRepository } from "../../domain/repositories/IUserRepository";
-export class UpdateUserTopcis {
-  constructor(private readonly userRepository: IUserRepository) {}
-  async execute(id: string, topics: string[]) {
-    await this.userRepository.updateUserTopics(id, topics);
-  }
-}
