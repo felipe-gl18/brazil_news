05ef847f0080102ff89008b9fc7d31edef6072ad
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-23 21:45:50 -0300

    chore: add dateService and calculateNextDeliveryAt and adding nextDeliveryAt logic

--- src/application/useCases/CreateUser.ts
+++ src/application/useCases/CreateUser.ts
@@ -3,11 +3,15 @@ import { IUserRepository } from "../../domain/repositories/IUserRepository";
 import { Email } from "../../domain/valueObjects/Email.js";
 import { TelegramChatId } from "../../domain/valueObjects/TelegramChatId.js";
 import { ICryptoService } from "../services/ICryptoService.js";
+import { IDateService } from "../services/IDateService.js";
+import { CalculateNextDeliveryAt } from "./CalculateNextDeliveryAt.js";
 import { CreateUserDTO } from "./CreateUserDTO";
 export class CreateUser {
   constructor(
     private readonly userRepository: IUserRepository,
-    private readonly cryptoService: ICryptoService
+    private readonly cryptoService: ICryptoService,
+    private readonly dateService: IDateService,
+    private readonly calculateNextDeliveryAt: CalculateNextDeliveryAt
   ) {}
   async execute(data: CreateUserDTO) {
     const email = new Email(data.email);
@@ -20,13 +24,19 @@ export class CreateUser {
       ? this.cryptoService.encrypt(telegramChatId.valueOf)
       : undefined;
 
+    const deliveryTime = new Date(data.deliveryTime);
     const user = new User({
       name: data.name,
       email,
       telegramChatId,
       topics: data.topics,
-      deliveryTime: new Date(data.deliveryTime),
+      deliveryTime,
       timezone: data.timezone,
+      nextDeliveryAt: this.calculateNextDeliveryAt.execute(
+        this.dateService.now(),
+        deliveryTime,
+        data.timezone
+      ),
     });
 
     await this.userRepository.create(user, encryptedTelegramChatId);
