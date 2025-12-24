dcd58a34f6995422b5bd21e5cba29f8329ceb387
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-23 21:58:49 -0300

    fix: remove timezone options

--- src/infra/services/NodeCronSchedulerService.ts
+++ src/infra/services/NodeCronSchedulerService.ts
@@ -2,11 +2,7 @@ import { IScheduler } from "../../application/services/IScheduler";
 import cron from "node-cron";
 
 export class NodeCronSchedulerService implements IScheduler {
-  schedule(
-    expression: string,
-    timezone: string,
-    task: () => Promise<void>
-  ): void {
-    cron.schedule(expression, task, { timezone });
+  schedule(expression: string, task: () => Promise<void>): void {
+    cron.schedule(expression, task);
   }
 }
