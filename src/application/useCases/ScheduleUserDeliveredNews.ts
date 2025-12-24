a0a44438a2b4dd599857cbb53b008d4e8d8e0af4
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-23 21:48:36 -0300

    feat: remove timezone from the global cron job
    add dateService and calculateNextDeliveryAt

--- src/application/useCases/ScheduleUserDeliveredNews.ts
+++ src/application/useCases/ScheduleUserDeliveredNews.ts
@@ -1,16 +1,32 @@
 import { IUserRepository } from "../../domain/repositories/IUserRepository";
+import { IDateService } from "../services/IDateService";
 import { IQueueService } from "../services/IQueueService";
 import { IScheduler } from "../services/IScheduler";
+import { CalculateNextDeliveryAt } from "./CalculateNextDeliveryAt";
 
 export class ScheduleUserDeliveredNews {
   constructor(
     private readonly userRepository: IUserRepository,
     private readonly scheduler: IScheduler,
-    private readonly queueService: IQueueService
+    private readonly queueService: IQueueService,
+    private readonly dateService: IDateService,
+    private readonly calculateNextDeliveryAt: CalculateNextDeliveryAt
   ) {}
   async execute(): Promise<void> {
-    this.scheduler.schedule("*/1 * * * *", "America/Sao_Paulo", async () => {
+    this.scheduler.schedule("*/1 * * * *", async () => {
       const now = new Date();
+      const users = await this.userRepository.findUsersToNotify(now);
+      if (!users) return;
+      for (const user of users) {
+        await this.queueService.addJob(user.id!);
+        const nextDeliveryAt = this.calculateNextDeliveryAt.execute(
+          this.dateService.now(),
+          user.deliveryTime,
+          user.timezone
+        );
+        user.setNextDeliveryAt(nextDeliveryAt);
+        await this.userRepository.save(user);
+      }
     });
   }
 }
