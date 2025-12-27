af785c03601bf0acf7e934fc9f28cba70290b81c
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-27 18:22:38 -0300

    feat: add parseTimeString abstraction implementation

--- src/infra/services/SystemDateService.ts
+++ src/infra/services/SystemDateService.ts
@@ -4,4 +4,7 @@ export class SystemDateService implements IDateService {
   now() {
     return new Date();
   }
+  parseTimeString(timeString: string): Date {
+    return new Date(timeString);
+  }
 }
