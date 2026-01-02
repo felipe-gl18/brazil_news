cf709b1c37d840db89630c6c991f699ee4d6c2ba
Felipe Lino <felipegadelha2004@gmail.com>
2026-01-02 20:51:13 -0300

    update: add update route

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -2,12 +2,14 @@ import express from "express";
 import "dotenv/config";
 import { registerUserRoute } from "./routes/registerUser.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import { updateUserRoute } from "./routes/UpdateUser.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/register", registerUserRoute);
+app.use("/update", updateUserRoute);
 
 app.use(errorHandler);
 
