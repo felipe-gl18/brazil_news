51c5d553b61924a9baa601979f7bd584ba5c55d7
Felipe Lino <felipegadelha2004@gmail.com>
2026-01-02 20:00:35 -0300

    fix: removing error handling
    add next function so the errorHandler middleware is triggered

--- src/infra/http/controllers/RegisterUser.ts
+++ src/infra/http/controllers/RegisterUser.ts
@@ -1,33 +1,15 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { CreateUser } from "../../../application/useCases/CreateUser.js";
-import { EmailAlreadyInUseError } from "../../../domain/erros/EmailAlreadyInUseError.js";
-
 export class RegisterUser {
   constructor(private readonly createUser: CreateUser) {}
-  async handle(req: Request, res: Response) {
+  async handle(req: Request, res: Response, next: NextFunction) {
     try {
       await this.createUser.execute(req.body);
       return res
         .status(201)
         .send({ success: true, message: "User registered successfully" });
     } catch (error) {
-      if (error instanceof EmailAlreadyInUseError)
-        return res
-          .status(409)
-          .send({ success: false, error: error.name, message: error.message });
-
-      if (error instanceof Error)
-        return res.status(500).json({
-          success: false,
-          error: "InternalServerError",
-          message: error.message,
-        });
-
-      return res.status(500).json({
-        success: false,
-        error: "InternalServerError",
-        message: "An unexpected error occurred",
-      });
+      next(error);
     }
   }
 }
