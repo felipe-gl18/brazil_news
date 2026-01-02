b4255c32e15c08930f6bb92d9ff997a9f5640a13
Felipe Lino <felipegadelha2004@gmail.com>
2026-01-01 22:10:15 -0300

    feat: http main file
    it starts the api, and also setup configuration

---
+++ src/infra/http/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import "dotenv/config";
+import { registerUserRoute } from "./routes/registerUser.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/register", registerUserRoute);
+
+app.use(errorHandler);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`The server is running on port ${process.env.PORT || 3000}`);
+});
