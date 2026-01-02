0bca752490630cfa609f16e059e53af693dce7a4
Felipe Lino <felipegadelha2004@gmail.com>
2026-01-02 20:01:36 -0300

    feat: errorHandler middleware
    middleware to identify if the error is at application or domain level

--- src/infra/http/middlewares/errorHandler.ts
+++ src/infra/http/middlewares/errorHandler.ts
@@ -1,19 +1,29 @@
 import { NextFunction, Request, Response } from "express";
-import { UserApplicationError } from "../../../application/erros/UserApplicationError";
-
+import { ApplicationError } from "../../../application/erros/ApplicationError.js";
+import { DomainError } from "../../../domain/erros/DomainError.js";
 export function errorHandler(
   error: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  if (error instanceof UserApplicationError)
+  console.log(error, "errorHandler");
+
+  if (error instanceof ApplicationError)
     return res.status(error.statusCode).json({
       success: false,
       error: error.name,
       message: error.message,
     });
 
+  if (error instanceof DomainError) {
+    console.log(error);
+    return res.status(400).json({
+      success: false,
+      error: error.name,
+      message: error.message,
+    });
+  }
   return res.status(500).json({
     success: false,
     error: "InternalServerError",
