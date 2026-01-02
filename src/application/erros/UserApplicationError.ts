6a7c0862a5e76ef6e3cc59e4327323eb5690d8a7
Felipe Lino <felipegadelha2004@gmail.com>
2026-01-02 20:03:02 -0300

    chore: delete UserApplicationError.ts

--- src/application/erros/UserApplicationError.ts
+++
@@ -1,7 +0,0 @@
-export abstract class UserApplicationError extends Error {
-  abstract statusCode: number;
-  constructor(message: string, cause?: unknown) {
-    super(message, { cause });
-    this.name = `UserApplicationError`;
-  }
-}
