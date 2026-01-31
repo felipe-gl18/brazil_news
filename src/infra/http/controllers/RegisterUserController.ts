484c31829e727a88af4a53aa27e93efe26bd050e
Felipe Lino <felipegadelha2004@gmail.com>
2026-01-30 23:56:09 -0300

    fix: redirecting the user to home page instead of returning json
    properly returning feedback with erros and filled values

--- src/infra/http/controllers/RegisterUserController.ts
+++ src/infra/http/controllers/RegisterUserController.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { CreateUser } from "../../../application/useCases/CreateUser.js";
+import { DomainError } from "../../../domain/erros/DomainError.js";
+import { ApplicationError } from "../../../application/erros/ApplicationError.js";
 export class RegisterUserController {
   constructor(private readonly createUser: CreateUser) {}
   async handle(req: Request, res: Response, next: NextFunction) {
@@ -9,6 +11,11 @@ export class RegisterUserController {
         .status(201)
         .send({ success: true, message: "User registered successfully" });
     } catch (error) {
+      if (error instanceof DomainError || error instanceof ApplicationError)
+        return res.status(400).render("home", {
+          errors: { [error.field || "form"]: error.message },
+          values: req.body,
+        });
       next(error);
     }
   }
