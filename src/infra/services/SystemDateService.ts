5288f989c3e788b146557717fa29e78a610b261a
Felipe Lino <felipegadelha2004@gmail.com>
2026-01-31 00:05:18 -0300

    fix: add proper way to parse HH:mm format into a valid Date variable with timezone

--- src/infra/services/SystemDateService.ts
+++ src/infra/services/SystemDateService.ts
@@ -1,10 +1,14 @@
+import { TZDate } from "@date-fns/tz";
 import { IDateService } from "../../application/services/IDateService";
 
 export class SystemDateService implements IDateService {
   now() {
     return new Date();
   }
-  parseTimeString(timeString: string): Date {
-    return new Date(timeString);
+  parseTimeString(timeString: string, timezone: string): Date {
+    const [hours, minutes] = timeString.split(":").map(Number);
+    const date = new TZDate(new Date(), timezone);
+    date.setHours(hours, minutes, 0, 0);
+    return date;
   }
 }
