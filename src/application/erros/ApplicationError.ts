ba8fa08ba2d5e0f02d6ad7a7ba6d0a5bdd47f010
Felipe Lino <felipegadelha2004@gmail.com>
2026-01-31 00:01:36 -0300

    change: add field property
    property needed to give a proper feedback when filling the register form

--- src/application/erros/ApplicationError.ts
+++ src/application/erros/ApplicationError.ts
@@ -1,5 +1,6 @@
 export abstract class ApplicationError extends Error {
   abstract statusCode: number;
+  field?: string;
   constructor(message: string, cause?: unknown) {
     super(message, { cause });
     this.name = `UserApplicationError`;
--- src/application/erros/EmailAlreadyInUseError.ts
+++ src/application/erros/EmailAlreadyInUseError.ts
@@ -1,6 +1,7 @@
 import { ApplicationError } from "./ApplicationError.js";
 export class EmailAlreadyInUseError extends ApplicationError {
   statusCode = 409;
+  field = "email";
   constructor(email: string) {
     super(`Email already in use: ${email}`);
     this.name = "EmailAlreadyInUseError";
