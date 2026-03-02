4b35594a71e94778ea37b75ce7688e03366ded65
Felipe Lino <felipegadelha2004@gmail.com>
2026-03-02 12:02:04 -0300

    chore: add generateRandomToken method

--- src/application/services/ICryptoService.ts
+++ src/application/services/ICryptoService.ts
@@ -2,4 +2,5 @@ import { EncrytedPayload } from "../dtos/CryptoServiceDTO";
 export interface ICryptoService {
   encrypt(value: string): EncrytedPayload;
   decrypt(payload: EncrytedPayload): string;
+  generateRandomToken(): string;
 }
