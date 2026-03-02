e9edb1210f59af3e2d226363f59e8faed4bfb0a9
Felipe Lino <felipegadelha2004@gmail.com>
2026-03-02 12:04:58 -0300

    feat: add update-account route

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -2,9 +2,10 @@ import express from "express";
 import "dotenv/config";
 import { registerUserRoute } from "./routes/registerUser.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
-import { updateUserRoute } from "./routes/UpdateUser.js";
+import { updateUserRoute } from "./routes/updateUser.js";
 import setupHandlebarsEngine from "./view-engine/handlebars.engine.js";
 import "../../index.js";
+import { updateAccountViewRoute } from "./routes/updateAccountView.js";
 
 export const app = express();
 
@@ -15,9 +16,13 @@ setupHandlebarsEngine(app);
 
 app.use("/register", registerUserRoute);
 app.use("/update", updateUserRoute);
+app.use("/update-account", updateAccountViewRoute);
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+app.get("/update-account", (req, res) => {
+  res.render("update");
 });
 
 app.use(errorHandler);
