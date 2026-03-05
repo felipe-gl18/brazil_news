7e97d4a010af2f7efb9848aece0e7f3ffc45dcd1
Felipe Lino <felipegadelha2004@gmail.com>
2026-03-04 23:23:57 -0300

    feat: returning an successfully updated page

--- src/infra/http/controllers/UpdateUserController.ts
+++ src/infra/http/controllers/UpdateUserController.ts
@@ -1,14 +1,21 @@
 import { NextFunction, Request, Response } from "express";
-import { UpdateUser } from "../../../application/useCases/UpdateUser";
+import { UpdateUser } from "../../../application/useCases/UpdateUser.js";
+import { DomainError } from "../../../domain/erros/DomainError.js";
+import { ApplicationError } from "../../../application/erros/ApplicationError.js";
 export class UpdateUserController {
   constructor(private readonly updateUser: UpdateUser) {}
   async handle(req: Request, res: Response, next: NextFunction) {
     try {
-      await this.updateUser.execute(req.params.id, req.body);
+      await this.updateUser.execute(req.params.token, req.body);
       return res
-        .status(200)
-        .send({ success: true, message: "User updated successfully" });
+        .status(201)
+        .render("updated-successfully", { values: req.body });
     } catch (error) {
+      if (error instanceof DomainError || error instanceof ApplicationError)
+        return res.status(400).render("update", {
+          errors: { [error.field || "form"]: error.message },
+          values: req.body,
+        });
       next(error);
     }
   }
