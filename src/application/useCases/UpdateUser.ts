3e079c4af187b2017fece489979abac310d27f9c
Felipe Lino <felipegadelha2004@gmail.com>
2026-03-04 23:20:42 -0300

    fix: replacing id for token
    adding nextDeliveryAt logic

--- src/application/useCases/UpdateUser.ts
+++ src/application/useCases/UpdateUser.ts
@@ -1,24 +1,40 @@
+import { ITokenRepository } from "../../domain/repositories/ITokenRepository";
 import { IUserRepository } from "../../domain/repositories/IUserRepository";
 import { Email } from "../../domain/valueObjects/Email.js";
 import { IDateService } from "../services/IDateService";
+import { CalculateNextDeliveryAt } from "./CalculateNextDeliveryAt";
 export class UpdateUser {
   constructor(
     private readonly userRepository: IUserRepository,
+    private readonly tokenRepository: ITokenRepository,
+    private readonly calculateNextDeliveryAt: CalculateNextDeliveryAt,
     private readonly dateService: IDateService,
   ) {}
-  async execute(id: string, data: UpdateUserDTO) {
-    const foundUser = await this.userRepository.findById(id);
-    if (data.name) foundUser.setName(data.name);
-    if (data.email) foundUser.setEmail(new Email(data.email));
+  async execute(token: string, data: UpdateUserDTO) {
+    const foundUser = await this.tokenRepository.findByToken(token);
+    if (data.name) foundUser!.setName(data.name);
+    if (data.email) foundUser!.setEmail(new Email(data.email));
     if (data.deliveryTime)
-      foundUser.setDeliveryTime(
+      foundUser!.setDeliveryTime(
         this.dateService.parseTimeString(
           data.deliveryTime,
-          data.timezone || foundUser.timezone,
+          data.timezone || foundUser!.timezone,
         ),
       );
-    if (data.timezone) foundUser.setTimezone(data.timezone);
-    if (data.topics) foundUser.setTopics(data.topics);
-    await this.userRepository.save(foundUser);
+    if (data.timezone) foundUser!.setTimezone(data.timezone);
+    if (data.topics) foundUser!.setTopics(data.topics);
+    const deliveryTime = this.dateService.parseTimeString(
+      data.deliveryTime!,
+      data.timezone!,
+    );
+    foundUser?.setNextDeliveryAt(
+      this.calculateNextDeliveryAt.execute(
+        this.dateService.now(),
+        deliveryTime,
+        data.timezone!,
+      ),
+    );
+    await this.userRepository.save(foundUser!);
+    await this.tokenRepository.deleteToken(token);
   }
 }
