44954a70b379f78ac24bb917eda7c1cd4dfd6343
Felipe Lino <felipegadelha2004@gmail.com>
2026-03-04 23:15:17 -0300

    feat: add parseDateToString method

--- src/application/services/IDateService.ts
+++ src/application/services/IDateService.ts
@@ -1,4 +1,5 @@
 export interface IDateService {
   now(date?: string): Date;
   parseTimeString(timeString: string, timezone: string): Date;
+  parseDateToString(date: Date, timezone: string): string;
 }
