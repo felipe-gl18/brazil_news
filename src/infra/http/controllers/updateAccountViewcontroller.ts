9082c2dae3f533c9838bf18e77208a5375188266
Felipe Lino <felipegadelha2004@gmail.com>
2026-03-04 23:23:23 -0300

    feat: returning values with formatted fields so we can full fill the update form with existing data

--- src/infra/http/controllers/updateAccountViewcontroller.ts
+++ src/infra/http/controllers/updateAccountViewcontroller.ts
@@ -2,15 +2,30 @@ import { NextFunction, Request, Response } from "express";
 import { DomainError } from "../../../domain/erros/DomainError.js";
 import { ApplicationError } from "../../../application/erros/ApplicationError.js";
 import { FindUserByToken } from "../../../application/useCases/FindUserByToken.js";
+import { IDateService } from "../../../application/services/IDateService.js";
 
 export class UpdateAccountViewController {
-  constructor(private readonly findUserByToken: FindUserByToken) {}
+  constructor(
+    private readonly findUserByToken: FindUserByToken,
+    private readonly systemDateService: IDateService,
+  ) {}
   async handle(req: Request, res: Response, next: NextFunction) {
     try {
       const token = req.query.token as string;
       const user = await this.findUserByToken.execute(token);
-      console.log(user);
-      return res.status(200).render("update", { token, values: user });
+      return res.status(200).render("update", {
+        token,
+        values: {
+          name: user?.name,
+          email: user?.email.valueOf,
+          deliveryTime: this.systemDateService.parseDateToString(
+            user?.deliveryTime!,
+            user?.timezone!,
+          ),
+          timezone: user?.timezone,
+          topics: user?.topics,
+        },
+      });
     } catch (error) {
       if (error instanceof DomainError || error instanceof ApplicationError)
         return res.status(400).render("home", {
