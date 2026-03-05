6871ef057b0ff462fe35dbf3e56d04186da31c74
Felipe Lino <felipegadelha2004@gmail.com>
2026-03-04 23:21:19 -0300

    fix: findByToken will return an User

--- src/domain/repositories/ITokenRepository.ts
+++ src/domain/repositories/ITokenRepository.ts
@@ -1,11 +1,7 @@
+import { User } from "../entities/User";
+
 export interface ITokenRepository {
   save(token: string, userId: string, expiresAt: Date): Promise<void>;
-  findByToken(token: string): Promise<{
-    name: string;
-    email: string;
-    deliveryTime: Date;
-    timezone: string;
-    topics: string[];
-  } | null>;
-  deleteByToken(token: string): Promise<void>;
+  findByToken(token: string): Promise<User | null>;
+  deleteToken(token: string): Promise<void>;
 }
