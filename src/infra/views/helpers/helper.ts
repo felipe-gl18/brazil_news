21ffcd3ba7e2357f9d2bbc0e1a9504092d24fc6d
Felipe Lino <felipegadelha2004@gmail.com>
2026-03-18 19:34:41 -0300

    feat: eq helper to verify the selected language

--- src/infra/views/helpers/helper.ts
+++ src/infra/views/helpers/helper.ts
@@ -2,6 +2,7 @@ import handlebars from "handlebars";
 handlebars.registerHelper("includes", function (array, value) {
   return array && array.includes(value);
 });
+handlebars.registerHelper("eq", (a: string, b: string) => a === b);
 handlebars.registerHelper("join", function (array, separator) {
   return Array.isArray(array) ? array.join(separator) : "";
 });
