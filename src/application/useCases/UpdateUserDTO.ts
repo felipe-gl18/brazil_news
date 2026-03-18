5af61740c35d3b7aa02244c0bab3e329b9ea08f4
Felipe Lino <felipegadelha2004@gmail.com>
2026-03-18 19:26:47 -0300

    chore: UpdateUserDto is now expecting optional language

--- src/application/useCases/UpdateUserDTO.ts
+++ src/application/useCases/UpdateUserDTO.ts
@@ -1,7 +1,10 @@
-interface UpdateUserDTO {
+import { Language } from "../../domain/entities/User";
+
+export interface UpdateUserDTO {
   name?: string;
   email?: string;
   topics?: string[];
   timezone?: string;
   deliveryTime?: string;
+  language?: Language;
 }
