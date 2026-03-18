4e6e1b6e98eafb3043a1976471844b03dbf2fe43
Felipe Lino <felipegadelha2004@gmail.com>
2026-03-18 19:26:12 -0300

    feat: add notificationDispatcherService
    add translationService

--- src/application/useCases/SendRSSNewsToUser.ts
+++ src/application/useCases/SendRSSNewsToUser.ts
@@ -2,20 +2,24 @@ import { News } from "../../domain/entities/News.js";
 import { IDeliveredNewsRepository } from "../../domain/repositories/IDeliveredNewsRepository";
 import { IUserRepository } from "../../domain/repositories/IUserRepository";
 import { NewsFilterService } from "../../domain/services/NewsFilterService.js";
+import { languages } from "../../utils/languages.js";
 import { RSSNewsMapper } from "../mappers/RSSNewsMapper.js";
 import { IFetchNewsService } from "../services/IFetchNewsService";
 import { INotificationService } from "../services/INotificationService";
+import { ITranslationService } from "../services/ITranslationService.js";
 import { SendUpdateAccountLink } from "./SendUpdateAccountLink.js";
 export class SendRSSNewsToUser {
   constructor(
     private readonly userRepository: IUserRepository,
     private readonly deliveredNewsRepository: IDeliveredNewsRepository,
     private readonly fetchNewsService: IFetchNewsService,
-    private readonly notificationService: INotificationService,
+    private readonly notificationDispatcherService: INotificationService,
     private readonly sendUpdateAccountLink: SendUpdateAccountLink,
+    private readonly translationService: ITranslationService,
   ) {}
   async execute(userId: string) {
     const foundUser = await this.userRepository.findById(userId);
+    const language = foundUser.language;
     const fetchedNewsDTOs = await this.fetchNewsService.fetchLatestNews(
       foundUser!.topics,
     );
@@ -32,8 +36,28 @@ export class SendRSSNewsToUser {
       telegramChatId: foundUser!.telegramChatId?.valueOf,
       updateAccountLink,
     };
-    await this.notificationService.notify({
-      news: releventNews,
+
+    const translatedNews = await Promise.all(
+      releventNews.map(async (item) => ({
+        content: await this.translationService.translate(
+          item.content,
+          foundUser.language,
+        ),
+        title: await this.translationService.translate(
+          item.title,
+          foundUser.language,
+        ),
+        publishedAt: item.publishedAt.toLocaleDateString(
+          `${languages[language].value}`,
+          languages[language].dateFormat,
+        ),
+        link: item.link,
+        topic: item.topic,
+      })),
+    );
+
+    await this.notificationDispatcherService.notify({
+      news: translatedNews,
       recipient,
     });
     await this.deliveredNewsRepository.saveMany(deliveredNews);
