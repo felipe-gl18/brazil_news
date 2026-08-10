2fc6a7666329d35a63eb4d35882c73f9aa9e006f
Felipe Lino <felipegadelha2004@gmail.com>
2026-08-10 20:07:11 -0300

    feat: AudioNotificationDispatcherService implementation

---
+++ src/infra/services/AudioNotificationDispatcherService.ts
@@ -0,0 +1,16 @@
+import { IAudioNotificationService } from "../../application/services/IAudioNotificationService";
+
+export class AudioNotificationDispatcherService implements IAudioNotificationService {
+  constructor(private readonly services: IAudioNotificationService[]) {}
+  async sendAudio(
+    recipient: string,
+    audio: Buffer,
+    caption?: string,
+  ): Promise<void> {
+    await Promise.all(
+      this.services.map((service) =>
+        service.sendAudio(recipient, audio, caption),
+      ),
+    );
+  }
+}
