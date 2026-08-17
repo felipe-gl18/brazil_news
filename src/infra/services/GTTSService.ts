e5d272905a376351ed7499c08be887a22dc39aab
Felipe Lino <felipegadelha2004@gmail.com>
2026-08-17 15:44:31 -0300

    chore: language will be defined at the method instead of the constructor

--- src/infra/services/GTTSService.ts
+++ src/infra/services/GTTSService.ts
@@ -2,14 +2,15 @@ import GTTS from "gtts";
 import { ITextToSpeechService } from "../../application/services/ITextToSpeechService";
 
 export class GTTSService implements ITextToSpeechService {
-  constructor(private readonly language: string = "pt-br") {}
-
-  async textToSpeech(text: string): Promise<Buffer> {
+  async textToSpeech(
+    text: string,
+    language: string = "pt-br",
+  ): Promise<Buffer> {
     if (!text.trim()) {
       throw new Error("Text cannot be empty");
     }
 
-    const gtts = new GTTS(text, this.language);
+    const gtts = new GTTS(text, language);
 
     return new Promise<Buffer>((resolve, reject) => {
       const chunks: Buffer[] = [];
